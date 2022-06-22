b681240e5f954158d13c2596c0408f659813c861
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-06-22 10:11:13 -0400

    components completed App component renders SearchBar and BusinessList components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,19 @@
-import logo from "/Users/olesiati/Desktop/greateats/src/logo.svg";
-
+import React, { Component } from "react";
+// import logo from "/Users/olesiati/Desktop/greateats/src/logo.svg";
 import "./App.css";
+import BusinessList from "../BusinessList/BusinessList.js";
+import SearchBar from "../SearchBar/SearchBar.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Olesia! Learn React.
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>GreatEats</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
 }
 
 export default App;
